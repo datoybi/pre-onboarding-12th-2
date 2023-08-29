c75d71a2931af3d667d0560e9bb0ef26acd008cc
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-08-29 20:18:34 +0900

    Refector: 불필요한 코드 제거

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
+import App from './pages/App/App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
